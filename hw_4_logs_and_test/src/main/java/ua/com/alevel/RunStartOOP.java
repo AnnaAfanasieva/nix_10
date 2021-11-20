86bf7a17a357b15086d2677f5f876bdcff3be9b7
egorfuntusov <funtushan@gmail.com>
2021-11-20 07:07:11 +0200

    add review

--- hw_4_logs_and_test/src/main/java/ua/com/alevel/RunStartOOP.java
+++ hw_4_logs_and_test/src/main/java/ua/com/alevel/RunStartOOP.java
@@ -3,6 +3,7 @@ package ua.com.alevel;
 import ua.com.alevel.controller.GroupController;
 
 public class RunStartOOP {
+
     public static void main(String[] args) {
         new GroupController().run();
     }
