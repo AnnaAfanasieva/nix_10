4d32490e76784379676c15285ebbd6ffed102805
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-11-25 23:48:28 +0200

    add first task and run class for module

---
+++ module_2/src/main/java/ua/com/alevel/RunModule.java
@@ -0,0 +1,17 @@
+package ua.com.alevel;
+
+import ua.com.alevel.date.TransformDates;
+import ua.com.alevel.name.FindFirstUniqueName;
+
+public class RunModule {
+    public static void main(String[] args) {
+        String separator = "_________________________________________________________________";
+        System.out.println(separator + "\nTask 1");
+        TransformDates.main(null);
+        System.out.println("Task 1 выполнен. Проверьте файл \"dateOutput.txt\"");
+        System.out.println(separator + "\nTask 2");
+        FindFirstUniqueName.main(null);
+        System.out.println(separator + "\nTask 3");
+        //ClassForTask3
+    }
+}
