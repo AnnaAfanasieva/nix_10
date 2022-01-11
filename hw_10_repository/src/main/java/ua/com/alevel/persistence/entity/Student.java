a49557e170a2a344620198503134bdcd0ccfc2bf
AnnaAfanasieva <aniafanasieva24@gmail.com>
2022-01-11 16:22:05 +0200

    add working project

--- hw_10_repository/src/main/java/ua/com/alevel/persistence/entity/Student.java
+++ hw_10_repository/src/main/java/ua/com/alevel/persistence/entity/Student.java
@@ -12,8 +12,6 @@ public class Student extends BaseEntity{
     private int age;
 
     @ManyToMany(cascade = {
-            CascadeType.PERSIST,
-            CascadeType.REFRESH,
             CascadeType.MERGE
     })
     @JoinTable(
