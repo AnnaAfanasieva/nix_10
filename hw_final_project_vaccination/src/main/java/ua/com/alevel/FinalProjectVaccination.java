25f59c0d31b22e9f60bcd3107804b02f5a24d8cc
AnnaAfanasieva <aniafanasieva24@gmail.com>
2022-01-19 02:27:38 +0200

    add module for final project

---
+++ hw_final_project_vaccination/src/main/java/ua/com/alevel/FinalProjectVaccination.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectVaccination {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectVaccination.class, args);
+    }
+
+}
